f9820eea25d844678c1ed91d36328b398dc80b41
uchel09 <ucelrussel77@gmail.com>
2024-08-26 23:38:51 +0800

    fixing setup2 mongo db

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"], 
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true,
   },
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB}`);
-
-    console.log("Connected successfully to mongoose server");
+    if (mongoose.connections[0].readyState) {
+      return true;
+    }
+    await mongoose.connect(process.env.MONGODB,{
+      serverSelectionTimeoutMS:30000,
+    });
+    console.log("mongodb connected");
   } catch (err) {
     console.log(`Error connecting to mongoose: ${err}`);
   }
