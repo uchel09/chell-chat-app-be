c5d4030b4cac84351ec698c03e1884627ef1f07d
uchel09 <ucelrussel77@gmail.com>
2024-08-26 23:02:34 +0800

    fixing setup mongodb

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB
-    );
+    await mongoose.connect(`${process.env.MONGODB}`);
 
     console.log("Connected successfully to mongoose server");
   } catch (err) {
